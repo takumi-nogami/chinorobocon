4aebeea3307e63e4516e30e871591be846cbd94d
Takumi Nogami <s16c1097nc@s.chibakoudai.jp>
2018-08-31 06:41:35 +0900

    rename a file

--- ros_start/scripts/commander_alpha.py
+++ ros_start/scripts/commander_alpha.py
@@ -28,53 +28,33 @@ class Alpha(object):
             self._instruct.publish(point)
 
     def order_callback(self, order_msg):             #モータ側のarduinoからフラッグと色情報を受け取った時の処理
-        #wait_msg = Int32()
-        if order_msg.x != self._info.x:              #同じ動きをしないかのチェック(同じ行動で衝突を防ぐため)
-           if order_msg.x == 1:
-              print "do fg = 1"
-              self._instruct.publish(order_msg)      #モータ側に送信
-              self._talk.publish(order_msg)          #ベータ（もう一つのraspi）に送信
-
-           elif order_msg.x == 2:
-              if self._info.x != 4:                    #外側優
-                 if order_msg.y != self._info.y:        #同じ色のボールを持っているか否かのチェック(同じゴールで衝突を防ぐため)
-                    print "do fg = 2"
-                    self._instruct.publish(order_msg)   #モータ側に送信
-                    self._talk.publish(order_msg)       #ベータ（もう一つのraspi）に送信                 
-                 else:               
-                     print "dwait!!"
-                     #wait_msg.data = 1
-                     self._wait.publish(order_msg)        #同じ行動をするようであるなら、モータ側に待機を命令
-
+        Move = order_msg.x
+        Ball = order_msg.y
+        Pass = order_msg.z
+        if Move != self._info.x:              #同じ動きをしないかのチェック(同じ行動で衝突を防ぐため)
+           if Move == 2:
+              if Pass == 0:
+                 print "Move = 2.1"  
+                 self._instruct.publish(order_msg)      #モータ側に送信
+                 self._talk.publish(order_msg)          #ベータ（もう一つのraspi）に送信
               else:
-                     print "wait!!"
-                     #wait_msg.data = 1
-                     self._wait.publish(order_msg)
-
-           elif order_msg.x == 3:
-              print "do fg = 3"
-              self._instruct.publish(order_msg)      #モータ側に送信
-              self._talk.publish(order_msg)          #ベータ（もう一つのraspi）に送信
-
-           elif order_msg.x == 4:
-              if self._info.x != 4:                    #外側優
-                 if order_msg.y != self._info.y:        #同じボールを持っているか否かのチェック(同じゴールで衝突を防ぐため)
-                    print "do fg = 4"
-                    self._instruct.publish(order_msg)   #モータ側に送信
-                    self._talk.publish(order_msg)       #ベータ（もう一つのraspi）に送信
-                 else:                                 
+                 J = pow(-1,self._info.x) * (self._info.y - Ball) * (2*self._info.z - 1)
+                 if J < 0:
+                    print "Move = 2.2"    
+                    self._instruct.publish(order_msg)      #モータ側に送信
+                    self._talk.publish(order_msg)          #ベータ（もう一つのraspi）に送信
+                 elif J >= 0:
                     print "wait!!"
-                    #wait_msg.data = 1
-                    self._wait.publish(order_msg)        #同じ行動をするようであるなら、モータ側に待機を命令
-              else:
-                 print "wait"
-                 #wait_msg.data = 1
-                 self._wait.publish(order_msg)
+                    self._wait.publish(order_msg)
 
+           else:
+              print "Move!!"  
+              self._instruct.publish(order_msg)      #モータ側に送信
+              self._talk.publish(order_msg)          #ベータ（もう一つのraspi）に送信
+                           
 
         else:               
            print "wait!!"                
-           #wait_msg.data = 1
            self._wait.publish(order_msg)              #同じ行動をするようであるなら、モータ側に待機を命令
            
            
@@ -82,6 +62,7 @@ class Alpha(object):
         print "conversation!!"
         self._info.x = conv_msg.x                    #どの行動状態にいるか
         self._info.y = conv_msg.y                    #どのボールを持っているか
+        self._info.z = conv_msg.z                    #どのPassを通っているか
 
 if __name__ == '__main__':
     rospy.init_node('alpha')
